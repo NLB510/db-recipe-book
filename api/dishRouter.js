d714982b381a194e2cd0e075be3377c06681837c
Nate Boyette <nateboyette@icloud.com>
2019-02-21 18:20:04 -0800

    Add endpoint to return ingredients by dish

--- api/dishRouter.js
+++ api/dishRouter.js
@@ -4,8 +4,11 @@ const db = require("../data/helpers/dishModel");
 
 const router = express.Router();
 
+
+
+// GET
 router.get('/', (req, res) => {
-  db.get()
+  db.getDishes()
     .then(dishes => {
       res.status(200).json(dishes);
     })
@@ -15,6 +18,48 @@ router.get('/', (req, res) => {
       });
     });
 })
+
+
+router.get("/:id", (req, res) => {
+  const {id} = req.params;
+  
+  db.getDish(id)
+    .then(dishIngredients => {
+      res.status(200).json(dishIngredients);
+    })
+    .catch(error => {
+      res.status(500).json({
+        error: "There was an error retrieving the dish data."
+      });
+    });
+});
+
+// POST
+
+router.post("/", (req, res) => {
+  const { dish_name } = req.body;
+  const dish = req.body;
+
+  if (!dish_name) {
+    res.status(400).json({
+      errorMessage: "Please include a dish name"
+    });
+  } else {
+    db.addDish(dish)
+    .then(dish => {
+      console.log(dish)
+      return res.status(201).json({
+        message: "Dish added successfully.",
+        dish
+      });
+    })
+    .catch(err => {
+      res.status(500).json({
+        error: "There was an error saving to the database."
+      });
+    });
+  }
+});
 
 
 module.exports = router;
--- data/helpers/dishModel.js
+++ data/helpers/dishModel.js
@@ -1,21 +1,43 @@
-const db = require('../dbConfig');
-
+const db = require("../dbConfig");
 
 module.exports = {
-  get,
+  getDishes,
+  addDish,
+  getById,
+  getDish
+};
+
+function getDishes() {
+  return db("dishes");
 }
 
-
-
-function get() {
-  return db('dishes')
+function getDish(id) {
+  return db
+    .select("ingredients.ingredient_name AS DishIngredients")
+    .from("dishes")
+    .innerJoin(
+      "recipe_ingredients",
+      "dishes.id",
+      "recipe_ingredients.recipe_id "
+    )
+    .innerJoin(
+      "ingredients",
+      "recipe_ingredients.ingredient_id",
+      "ingredients.id"
+    )
+    .where({ 'dishes.id': id });
 }
 
+function getById(id) {
+  return db("dishes")
+    .where({ id })
+    .first();
+}
 
 function addDish(dish) {
-  return('dishes')
-  .insert(dish)
-  .then(ids => {
+  return db("dishes")
+    .insert(dish)
+    .then(ids => {
       return getById(ids[0]);
     });
 }
